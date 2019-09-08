48323fe9a8f22f291dff71defe0cc747dcdf4fc7
wn1980 <w.n1980@outlook.com>
2019-09-08 10:33:07 +0700

    can call speech service

---
+++ turtlebot_tasks/scripts/voice_cmd.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import rospy
+
+from dialogflow_ros.srv import DialogflowService
+
+def cmd():
+	rospy.wait_for_service('voice_cmd')
+		
+	try:
+		voice_cmd = rospy.ServiceProxy('voice_cmd', DialogflowService)
+		results = voice_cmd()
+		
+		if results.success:
+			return results.result.action, results.result.query_text, results.result.fulfillment_text
+			
+	except rospy.ServiceException, e:
+		print "Service call failed: %s"%e
+		return None
+	
+	return None
+
+def say(s):
+	rospy.wait_for_service('speak')
+		
+	try:
+		speak = rospy.ServiceProxy('speak', DialogflowService)
+		results = speak(True, s)
+		
+		if results.success:
+			return True
+			
+	except rospy.ServiceException, e:
+		print "Service call failed: %s"%e
+		return False
+	
+	return False
+
+if __name__ == "__main__":
+	r = cmd()
+	
+	if r is not None:
+		#print(r)
+		a, q, f = r[0], r[1], r[2]
+		print(f)
+		say(f)
